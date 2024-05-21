3fe1eed62bc05faf6102cae9814700dfed8bd729
Sergei Koshelnikov <kosheklnikovsa@gmail.com>
2024-05-21 08:26:15 +0200

    added assets

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,15 @@
 import {createRoot} from "react-dom/client";
 import Button from "./components/button/button";
 import './styles.css'
+import logo from './assets/react.svg';
 
 const root = createRoot(document.getElementById('root'))
 //const i = 1;
 //i = ''
-root.render(<Button/>)
+root.render(
+    <div>
+        <Button/>
+        <div><img className='logo' src={logo} alt='Logo'/></div>
+        <div className='inline-log'></div>
+    </div>
+)
--- webpack.development.js
+++ webpack.development.js
@@ -2,5 +2,13 @@ const { merge} = require('webpack-merge');
 const commonConfig = require('./webpack.config')
 
 module.exports = merge(commonConfig, {
-    mode: "development"
+    mode: "development",
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader' ]
+            }
+        ]
+    }
 })
